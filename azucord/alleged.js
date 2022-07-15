3ee0900ada4407428263192c2fe6f02b531931c0
Lettuce Cobbler Twinkie <64616029+CSeriestechhero@users.noreply.github.com>
2022-07-14 19:47:13 -0700

    Update alleged.js

--- azucord/alleged.js
+++ azucord/alleged.js
@@ -1,6 +1,6 @@
 $.getJSON("https://api.ipify.org/?format=json", function(e) {
     console.log(e.ip);
-    if (e.ip === '98.167.96.150' || e.ip === '169.241.65.55' || e.ip === '67.220.180.82' || e.ip === '187.254.97.204') {
+    if (e.ip === '98.167.96.150' || e.ip === '169.241.65.55' || e.ip === '67.220.180.82' || e.ip === '187.254.97.204' || e.ip === '207.155.90.58') {
 console.log("bad ip")
 window.location.replace('alleged.html')
 } else {
